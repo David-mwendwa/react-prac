82ef301d4b2213398fdacfe1096ff2aa0cb0161c
David Mwendwa <klatzdavie19@gmail.com>
2021-11-01 17:32:25 +0300

    feat: add users list component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import React, {useState} from 'react'
 import AddUsers from "./components/Users/AddUser";
+import UsersList from "./components/Users/UsersList";
+
+const USERS = [
+  { id: 'u1', name: 'David', age: 25 },
+  { id: 'u2', name: 'John', age: 24 },
+  { id: 'u3', name: 'Kim', age: 17 },
+];
 
 function App() {
+  let [users, setUsers] = useState(USERS)
   return (
     <div>
       <AddUsers />
+      <UsersList users={users} />
     </div>
   );
 }
