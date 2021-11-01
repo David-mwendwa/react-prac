ffbca874f59cfc2af3510c9701e54865690e36b4
David Mwendwa <klatzdavie19@gmail.com>
2021-11-01 17:02:17 +0300

    feat: add validation and resetting logic

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,35 +1,49 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react';
 import Card from '../UI/Card';
-import Button from '../UI/Button'
-import classes from './AddUser.module.css'
+import Button from '../UI/Button';
+import classes from './AddUser.module.css';
 
 const AddUsers = (props) => {
-  const [enteredUsername, setEnteredUsername] = useState('')
-  const [enteredAge, setEnteredAge] = useState('')
+  const [enteredUsername, setEnteredUsername] = useState('');
+  const [enteredAge, setEnteredAge] = useState('');
 
   const addUserHandler = (event) => {
     event.preventDefault();
-    console.log(enteredAge, enteredUsername)
-  }
+    if (setEnteredUsername.trim().length || enteredAge.trim().length) return
+    if (+enteredAge < 1) return 
+    console.log(enteredAge, enteredUsername);
+    setEnteredUsername('');
+    setEnteredAge('');
+  };
 
-  const usernameChangeHandler = event => {
-    setEnteredUsername(event.target.value)
-  }
+  const usernameChangeHandler = (event) => {
+    setEnteredUsername(event.target.value);
+  };
   const ageChangeHandler = (event) => {
     setEnteredAge(event.target.value);
   };
-  
+
   return (
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor='username'>Username</label>
-        <input type='text' id='username' onChange={usernameChangeHandler} />
+        <input
+          type='text'
+          id='username'
+          value={enteredUsername}
+          onChange={usernameChangeHandler}
+        />
         <label htmlFor='age'>Age (years)</label>
-        <input type='number' id='age' onChange={ageChangeHandler} />
+        <input
+          type='number'
+          id='age'
+          value={enteredAge}
+          onChange={ageChangeHandler}
+        />
         <Button type='submit'>Add User</Button>
       </form>
     </Card>
   );
-}
+};
 
-export default AddUsers
+export default AddUsers;
